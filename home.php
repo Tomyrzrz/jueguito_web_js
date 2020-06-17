67b45787ed5031a5f327e83bfd1cb679079e4c29
Lupita-REY <65307470+Lupita-REY@users.noreply.github.com>
2020-06-17 13:37:51 -0500

    Update home.php

--- home.php
+++ home.php
@@ -3,7 +3,7 @@
     <title>Hola</title>
  </head>
   <body>
-    <h1> Hola mundo en GitHUb</h1>
+    <h1> Hola mundo en GitHUb y Git</h1>
    <p> Estamos haciendo una prueba de edicion de archivos </p>
   </body>
 

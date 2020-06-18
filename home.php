c58fdde1fa8eed85e73f4ad75a8bbf3f56dc7aa3
mina2019123 <65421169+mina2019123@users.noreply.github.com>
2020-06-18 14:01:48 -0500

    agregue nuevo parrafo
    sólo es un pequeño texto

--- home.php
+++ home.php
@@ -6,7 +6,9 @@
     <h1> Hola mundo en GitHUb</h1>
     <h1> Prueba 1 Lupita </h1>
    <p> Estamos haciendo una prueba de edicion de archivos </p>
-   <p> Este en mi parrafo soy Alejandro Fernandez </p>
+   <p> Este en mi parrafo soy Carmina Rojas</p>
+    <p> Cambio al nuevo proyecto jueguitoweb </p>
+   
    <h1>Quedate en casa</>
   </body>
 

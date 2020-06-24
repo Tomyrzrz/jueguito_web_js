953f2b576a38941e2467e379ac3fe029fbd565a0
Timoteo <ruiz.timo.ruiz@gmail.com>
2020-06-23 22:39:28 -0500

    SOy el user UTOM Class TI

--- home.php
+++ home.php
@@ -3,7 +3,7 @@
     <title>Hola</title>
   </head>
   <body>
-    <h1> Hola mundo en GitHUb</h1>
+    <h1> Hola mundo en GitHUb soy el usuario de UTOM CLASS TI.</h1>
 
     <h1>Quedate en casa</>
     

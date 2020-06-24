f472228d540d73c182c12a1e5ff22ef685b3e400
Softim <36336799+Tomyrzrz@users.noreply.github.com>
2020-06-23 22:32:25 -0500

    Cambio

--- home.php
+++ home.php
@@ -3,7 +3,7 @@
     <title>Hola</title>
   </head>
   <body>
-    <h1> Hola mundo en GitHUb Yo soy el colaborador UTOM Class TI</h1>
+    <h1> Hola mundo en GitHUb</h1>
 
     <h1>Quedate en casa</>
     
